d618bf47fbc0d0295b8c2471b536014df1a45f7f
Aiyoun Khan <aiyounkhan@Aiyouns-Mac-mini.local>
2023-11-05 23:18:16 +0600

    Users and blog general api made

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 8080
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9bsrsbd.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,6 +30,38 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+
+        const usersCollection = client.db('blogsDB').collection('users')
+        const blogsCollection = client.db('blogsDB').collection('blogs')
+
+        app.get('/users', async(req, res) => {
+            const cursor = usersCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/blogs', async (req, res) => {
+            const cursor = blogsCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/users/:id', async(req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await usersCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.get('/blogs/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await blogsCollection.findOne(query)
+            res.send(result)
+        })
+
+
+
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
@@ -37,7 +69,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        //await client.close();
     }
 }
 run().catch(console.dir);
